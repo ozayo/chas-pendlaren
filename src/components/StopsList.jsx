ad05189acbb7094bcd653dae69de129f05924b70
ozayo <ozay.ozdemir@gmail.com>
2024-08-29 14:37:03 +0200

    Updated to allow stop selection and display of timetable

--- src/components/StopsList.jsx
+++ src/components/StopsList.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import DeparturesList from './DeparturesList'; // show stops time tabel
 
 function StopsList({ coords }) {
   const [stops, setStops] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  //Select stop and show time table update
+  const [selectedStopId, setSelectedStopId] = useState(null); // selected stop 
+  const handleStopSelect = (id) => {
+    setSelectedStopId(id);
+  };
 
   useEffect(() => {
     if (!coords) return;
@@ -29,19 +35,35 @@ function StopsList({ coords }) {
 
   if (!coords) return null;
 
+  // OLD RENDER shows just nearby stops
+  // return (
+  //   <div>
+  //     {loading && <p>Loading stops...</p>}
+  //     {error && <p>{error}</p>}
+  //     <ul>
+  //       {stops.map((stop, index) => (
+  //         <li key={index}>
+  //           {stop.StopLocation ? `${stop.StopLocation.name} (${stop.StopLocation.id})` : 'Stop location data is not available.'}
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
+
+  // new render
   return (
     <div>
-      {loading && <p>Loading stops...</p>}
-      {error && <p>{error}</p>}
       <ul>
         {stops.map((stop, index) => (
-          <li key={index}>
-            {stop.StopLocation ? `${stop.StopLocation.name} (${stop.StopLocation.id})` : 'Stop location data is not available.'}
+          <li key={index} onClick={() => handleStopSelect(stop.StopLocation.id)}>
+            {stop.StopLocation.name}
           </li>
         ))}
       </ul>
+      {selectedStopId && <DeparturesList stopId={selectedStopId} />}
     </div>
   );
+
 }
 
 export default StopsList;
